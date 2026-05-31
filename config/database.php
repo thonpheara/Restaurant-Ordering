1db7686a6c45b6c48e395e1f0f0fd44766ef5fc4
thonpheara <thonpheara4@gmail.com>
2026-05-31 13:34:29 +0700

    Push for Deployment

--- config/database.php
+++ config/database.php
@@ -47,7 +47,7 @@ return [
             'strict'         => true,
             'engine'         => null,
             'options'        => extension_loaded('pdo_mysql') ? array_filter([
-                Pdo\Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                \PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
 
